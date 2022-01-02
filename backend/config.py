767559b498e77f5fdc194b503e2fe8f944c22d8e
vivy-c <cahyanivivy@gmail.com>
2022-01-02 10:52:00 +0700

    Imp(backend): add endpoint GET

--- backend/config.py
+++ backend/config.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-app.config["SQLALCHEMY_DATABSE_URI"] = "sqlite:///flask-react.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///flask-react.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,12 @@ from flask import Flask, jsonify, request
 from config import app, db
 from models import Contact
 
+@app.route("/contacts", methods=["GET"])
+def get_contacts(): 
+    contacts = Contact.query.all()
+    json_contacts = list(map(lambda x: x.to_json(), contacts))
+    return jsonify({"contacts": json_contacts})
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
