298963d313a4c4064efd2c90bfc6454f0d8ca290
vivy-c <cahyanivivy@gmail.com>
2022-01-03 10:30:00 +0700

    Imp(backend): add endpoint POST for create

--- backend/main.py
+++ backend/main.py
@@ -8,6 +8,28 @@ def get_contacts():
     json_contacts = list(map(lambda x: x.to_json(), contacts))
     return jsonify({"contacts": json_contacts})
 
+@app.route("/create_contact", methods=["POST"])
+def create_contact():
+    first_name = request.json.get("firstName")
+    last_name = request.json.get("lastName")
+    email = request.json.get("email")
+    
+    if not first_name or not last_name or not email:
+        return (
+            jsonify({"message": "Missing required fields"}), 
+            400,
+        )
+        
+    new_contact = Contact(first_name = first_name, last_name = last_name, email = email)
+    try:
+        db.session.add(new_contact)
+        db.session.commit()
+        return jsonify({"success": "Contact created successfully"}), 201
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
+    
+    return jsonify({"message": "Contact created!"}), 201
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
