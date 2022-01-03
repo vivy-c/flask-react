3c6b72ae349c39713d4edefc3c9910a6ed10dbab
vivy-c <cahyanivivy@gmail.com>
2022-01-03 15:00:00 +0700

    Imp(frontend): setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,14 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [contacts, setContacts] = useState([])
+
+  const fetchContacts = async () => {
+    const response = await fetch('http://localhost:5000/contacts')
+    const data = await response.json()
+    setContacts(data.contacts)
+    console.log(data.contacts)
+  }
   return (
     <>
       
