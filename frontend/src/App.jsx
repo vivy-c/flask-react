8c14d244b7e31da9fa412583e43916ce65be0fce
vivy-c <cahyanivivy@gmail.com>
2022-01-03 16:00:00 +0700

    Fix(frontend): fix BE url for getting logs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
   const [contacts, setContacts] = useState([])
 
+  useEffect(() => {
+    fetchContacts()
+  }, [])
+
   const fetchContacts = async () => {
-    const response = await fetch('http://localhost:5000/contacts')
+    const response = await fetch('http://127.0.0.1:5000/contacts')
     const data = await response.json()
     setContacts(data.contacts)
     console.log(data.contacts)
