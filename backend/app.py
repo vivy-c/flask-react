8de6643e7b3bfa0c1bf526a0eba84191845745c0
vivy-c <cahyanivivy@gmail.com>
2022-01-10 10:52:00 +0700

    Imp(backend): flask initial database migration

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 CORS(app)
@@ -9,3 +10,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///flask-react.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+if __name__ == '__main__':
+    app.run(debug=True)
